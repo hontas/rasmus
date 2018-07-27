546dafc794aaa5ebd619324f691e8934b4903635
hontas <lundin.pontus@gmail.com>
2018-07-28 00:35:56 +0200

    updated max.len setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "extends": "airbnb-base",
     rules: {
+      "max-len": [1, 120],
       "no-use-before-define": 0,
       "arrow-parens": ["error", "always"],
       "comma-dangle": 0
